8d920f8eb37576b5bc4ae0e846aec444a63b8d95
takethispie <takethispie@gmail.com>
2019-03-04 16:14:44 +0100

    first commit

---
+++ Perceptron.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace simple_ml
+{
+    public class Perceptron
+    {
+        public int[] Inputs, Outputs, Weights;
+
+        public Perceptron(int[] inputs, int[] weights) {
+            this.Inputs = inputs;
+        }
+    }
+}
