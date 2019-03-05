ebbac746a8cd665ec21d660e6ab7c2862f7aa4a8
Olivier <Olivier@OLIVIER-PC>
2019-03-05 18:46:52 +0100

    Added activation function + guessing function  + some tests

--- simple-ml-tests/PerceptronTests.cs
+++ simple-ml-tests/PerceptronTests.cs
@@ -1,14 +1,48 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using simple_ml;
 
 namespace simple_ml_tests
 {
     [TestClass]
     public class PerceptronTests
     {
+        private Perceptron _perceptron;
+
         [TestMethod]
-        public void TestMethod1()
+        public void WeightsShouldBeBetweenInclusiveNegativeOneAndInclusivePositiveOne()
         {
+            const int minimumWeight = -1;
+            const int maximumWeight = 1;
+            var inputs = new[] {1.0f, 1.5f, 0.6f};
+            _perceptron = new Perceptron(minimumWeight, maximumWeight);
+
+            foreach (var weight in _perceptron.Weights)
+            {
+                Assert.IsTrue(weight >= minimumWeight && weight <= maximumWeight);
+            }
+        }
+
+        [TestMethod]
+        public void GuessShouldBeEqualToNegativeOneOrPositiveOne()
+        {
+            const int minimumWeight = -1;
+            const int maximumWeight = 1;
+            var inputs = new[] { 1.0f, 1.5f, 0.6f };
+            _perceptron = new Perceptron(minimumWeight, maximumWeight);
+
+            int guess = _perceptron.Guess(inputs);
+
+            Assert.IsTrue(guess == minimumWeight || guess == maximumWeight);
+        }
+
+        [TestMethod]
+        public void OutputsShouldBeEmpty()
+        {
+            var inputs = new[] { 1.0f, 1.5f, 0.6f };
+            _perceptron = new Perceptron();
+
+            Assert.IsNull(_perceptron.Outputs);
         }
     }
 }
--- simple-ml/Perceptron.cs
+++ simple-ml/Perceptron.cs
@@ -4,10 +4,49 @@ namespace simple_ml
 {
     public class Perceptron
     {
-        public int[] Inputs, Outputs, Weights;
+        public float[] Weights { get; private set; }
+        public int[] Outputs;
 
-        public Perceptron(int[] inputs, int[] weights) {
-            Inputs = inputs;
+        public Perceptron(int minimumWeight = -1, int maximumWeight = 1)
+        {
+            RandomizeWeights(minimumWeight, maximumWeight);
+        }
+
+        /// <summary>
+        /// Computes the sum of each input with its respective weight
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <returns></returns>
+        public int Guess(float[] inputs)
+        {
+            float sum = 0f;
+
+            for (int i = 0; i < Weights.Length; i++)
+            {
+                sum += inputs[i] * Weights[i];
+            }
+
+            return Activate(sum);
+        }
+
+        /// <summary>
+        /// Rosenblatt rule
+        /// </summary>
+        /// <param name="sum"></param>
+        /// <returns></returns>
+        private static int Activate(float sum)
+        {
+            if (sum >= 0) return 1;
+
+            return -1;
+        }
+
+        private void RandomizeWeights(int minimum, int maximum)
+        {
+            for (int i = 0; i < Weights.Length; i++)
+            {
+                Weights[i] = new Random().Next(minimum, maximum);
+            }
         }
     }
 }
