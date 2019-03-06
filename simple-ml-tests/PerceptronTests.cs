ba1754a43899d3c66f6043779bc1b67ee51281ab
Olivier <Olivier@OLIVIER-PC>
2019-03-07 00:14:18 +0100

    Added mean of retrieving the class of each input "row" + added further unit testing

--- simple-ml-tests/PerceptronTests.cs
+++ simple-ml-tests/PerceptronTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using simple_ml;
 
@@ -8,7 +9,7 @@ namespace simple_ml_tests
     public class PerceptronTests
     {
         [TestMethod]
-        public void ShouldOutputExpectedResults()
+        public void ShouldOutputExpectedClasses()
         {
             var input = new[]{
                 new[] {1.0, 0.0},
@@ -17,20 +18,107 @@ namespace simple_ml_tests
                 new[] {2.0, 2.0}
             };
 
-            var expectedOutput = new double[] {-1, 1, -1, 1};
+            var expectedOutput = new double[] {-1, 1, -1, -1};
             const double learningRate = 0.1;
             const int nbEpochs = 1000;
             
-            double[] weights = Perceptron.CreateModel(2);
-            weights = Perceptron.TrainLinearClassification(weights, input, expectedOutput, learningRate, nbEpochs);
+            var model = Perceptron.TrainLinearClassifier(input, expectedOutput, learningRate, nbEpochs);
+            var classes = Perceptron.GetInputClasses(model, input, expectedOutput);
 
-            Console.WriteLine(Perceptron.ClassificationLinearInference(weights, input[0]));
-            Console.WriteLine(Perceptron.ClassificationLinearInference(weights, input[1]));
-            Console.WriteLine(Perceptron.ClassificationLinearInference(weights, input[2]));
-            Console.WriteLine(Perceptron.ClassificationLinearInference(weights, input[3]));
-            Console.WriteLine(weights[0]);
-            Console.WriteLine(weights[1]);
-            Console.WriteLine(weights[2]);
+            CollectionAssert.AreEqual(expectedOutput, classes);
+        }
+
+        [TestMethod]
+        public void ShouldSignWithNegativeOne()
+        {
+            double result = Perceptron.Sign(-59.0);
+
+            Assert.AreEqual(result, -1);
+        }
+
+        [TestMethod]
+        public void ShouldSignZeroWithPositiveOne()
+        {
+            double result = Perceptron.Sign(0.0);
+
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void ShouldSignWithPositiveOne()
+        {
+            double result = Perceptron.Sign(16.0);
+
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void ShouldCreateModelWithInputSizePlusOne()
+        {
+            var input = new[]{
+                new[] {1.0, 0.0},
+                new[] {0.0, 1},
+                new[] {1.0, 1.0},
+                new[] {2.0, 2.0}
+            };
+
+            int nbInputColumns = input[0].Length;
+            var model = Perceptron.CreateModel(nbInputColumns);
+
+            Assert.IsTrue(model.Length - 1 == nbInputColumns);
+        }
+
+        [TestMethod]
+        public void ShouldCreateModelWithRandomDoubleValues()
+        {
+            var input = new[]{
+                new[] {1.0, 0.0},
+                new[] {0.0, 1},
+                new[] {1.0, 1.0},
+                new[] {2.0, 2.0}
+            };
+
+            int nbInputColumns = input[0].Length;
+            var model = Perceptron.CreateModel(nbInputColumns);
+
+            CollectionAssert.AllItemsAreInstancesOfType(model, typeof(double));
+        }
+
+        [TestMethod]
+        public void ShouldCreateModelWithRandomDoubleValuesBetweenNegativeOneAndPositiveOne()
+        {
+            var input = new[]{
+                new[] {1.0, 0.0},
+                new[] {0.0, 1},
+                new[] {1.0, 1.0},
+                new[] {2.0, 2.0}
+            };
+
+            int nbInputColumns = input[0].Length;
+            var model = Perceptron.CreateModel(nbInputColumns);
+
+            bool hasIncorrectValue = model.Any(x => x < -1.0 || x > 2.0);
+
+            Assert.IsFalse(hasIncorrectValue);
+        }
+
+        [TestMethod]
+        public void ShouldHaveIncorrectValue()
+        {
+            var input = new[]{
+                new[] {1.0, 0.0},
+                new[] {0.0, 1},
+                new[] {1.0, 1.0},
+                new[] {2.0, 2.0}
+            };
+
+            int nbInputColumns = input[0].Length;
+            var model = Perceptron.CreateModel(nbInputColumns);
+            model[0] = 2.1;
+
+            bool hasIncorrectValue = model.Any(x => x < -1.0 || x > 2.0);
+
+            Assert.IsTrue(hasIncorrectValue);
         }
     }
 }
--- simple-ml/Perceptron.cs
+++ simple-ml/Perceptron.cs
@@ -2,22 +2,27 @@
 
 namespace simple_ml
 {
-    public class Perceptron
+    public static class Perceptron
     {
+        /// <summary>
+        /// Generates the model based on the number of columns in the input
+        /// </summary>
+        /// <param name="nbInputColumns">Number of columns in the input</param>
+        /// <returns>The model with random double values</returns>
         public static double[] CreateModel(int nbInputColumns)
         {
-            var weights = new double[nbInputColumns + 1];
+            var model = new double[nbInputColumns + 1];
 
-            for (int i = 0; i < weights.Length; i++)
+            for (int i = 0; i < model.Length; i++)
             {
-                weights[i] = new Random().NextDouble() * 2.0 - 1.0;
+                model[i] = new Random().NextDouble() * 2.0 - 1.0;
             }
 
-            return weights;
+            return model;
         }
 
         /// <summary>
-        /// Rosenblatt rule
+        /// Rosenblatt rule activation function
         /// </summary>
         /// <param name="sum"></param>
         /// <returns></returns>
@@ -28,19 +33,8 @@ namespace simple_ml
             return -1;
         }
 
-        public static int ClassificationLinearInference(double[] model, double[] input)
-        {
-            var total = model[0];
-
-            for (int i = 1; i <= input.Length; i++)
-            {
-                total += model[i] * input[i - 1];
-            }
-
-            return Sign(total);
-        }
-
-        private double RegressiveLinearInference(double[] model, double[] input)
+        [Obsolete]
+        public static double RegressiveLinearInference(double[] model, double[] input)
         {
             var total = model[0];
 
@@ -53,20 +47,39 @@ namespace simple_ml
         }
 
         /// <summary>
-        /// 
+        /// Applying the classifier linear inference formula
         /// </summary>
-        /// <param name="model">Tableau des poids</param>
-        /// <param name="inputs">Une dimension par paramètre tel que l'âge, le sexe, la taille, etc.</param>
-        /// <param name="expectedOutput"></param>
-        /// <param name="learningRate"></param>
-        /// <param name="nbEpochs">Nombre d'itérations</param>
-        public static double[] TrainLinearClassification(double[] model, double[][] inputs, double[] expectedOutput, double learningRate, int nbEpochs)
+        /// <param name="model">Associated model</param>
+        /// <param name="input">Associated input</param>
+        /// <returns>1 or -1 based on the Rosenblatt rule</returns>
+        public static int ClassifierLinearInference(double[] model, double[] input)
         {
+            var total = model[0];
+
+            for (int i = 1; i <= input.Length; i++)
+            {
+                total += model[i] * input[i - 1];
+            }
+
+            return Sign(total);
+        }
+
+        /// <summary>
+        /// Trains on a dataset input based on the linear classifier way
+        /// </summary>
+        /// <param name="inputs">One dimension per parameter like the age, the sex, the height, etc.</param>
+        /// <param name="expectedOutput">Represents the expected class for each input</param>
+        /// <param name="learningRate">Learning rate of the machine</param>
+        /// <param name="nbEpochs">Nombre d'itérations</param>
+        public static double[] TrainLinearClassifier(double[][] inputs, double[] expectedOutput, double learningRate = 0.1, int nbEpochs = 1000)
+        {
+            var model = CreateModel(inputs[0].Length);
+
             for (int i = 0; i < nbEpochs; i++)
             {
                 for (int k = 0; k < inputs.Length; k++)
                 {
-                    var gxk = ClassificationLinearInference(model, inputs[k]);
+                    var gxk = ClassifierLinearInference(model, inputs[k]);
 
                     var yk = expectedOutput[k];
 
@@ -78,6 +91,25 @@ namespace simple_ml
             }
 
             return model;
+        }
+
+        /// <summary>
+        /// Retrieves the class for each input
+        /// </summary>
+        /// <param name="model">Associated model</param>
+        /// <param name="input">Associated input</param>
+        /// <param name="expectedOutput">Expected classes for each input</param>
+        /// <returns>Input's classes</returns>
+        public static double[] GetInputClasses(double[] model, double[][] input, double[] expectedOutput)
+        {
+            var classes = new double[expectedOutput.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                classes[i] = ClassifierLinearInference(model, input[i]);
+            }
+
+            return classes;
         }
     }
 }
