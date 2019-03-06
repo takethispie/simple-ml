76f8b4645a7189d6c06dc728a5f689f7763345c6
takethispie <takethispie@gmail.com>
2019-03-06 12:17:48 +0100

    using nunit dotnet core template created basic and gate test

--- simple-ml/Perceptron.cs
+++ simple-ml/Perceptron.cs
@@ -8,11 +8,9 @@ namespace simple_ml
     {
         public double[] Weights;
         public double LearningRate, Threshold;
-        private double[] inputs;
 
         public Perceptron(int inputCount, double learningRate = 0.1, double threshold = 0.5) {
-            inputs = new double[inputCount];
-            Weights = new double[inputCount];
+            Weights = new double[inputCount+1];
             LearningRate = learningRate;
             Threshold = threshold;
         }
@@ -20,13 +18,15 @@ namespace simple_ml
         //on prend les entrée et les multiplie par leur poid respectif pour mettre dans un nouveau array
         //si la sum de toute le weighted input > que threshold (0,5) alors true sinon false
         // => seuil de d'activation
-        public bool Process(double[] inputs) => inputs.Zip(Weights, (v, weight) => v*weight).Sum() > Threshold ? true : false;
+        public bool Process(double[] inputs) => inputs
+                                                    .Zip(Weights, (v, weight) => v*weight)
+                                                    .Sum() > Threshold;
 
         public bool Learn(bool expectedResult, double[] inputs) {
             var res = Process(inputs);
             if(res == expectedResult) return res;
             double error = (expectedResult?1:0) - (res?1:0);
-            Weights = Weights.Zip(inputs, (weight, input) => weight + (LearningRate * error * input)).ToArray();
+            Weights = Weights.Zip(inputs, (weight, input) => weight + LearningRate * error * input).ToArray();
             return res;
         }
     }
