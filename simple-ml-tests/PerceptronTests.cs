bae7cedd6d6e99a38db9b62a45dfa6eb782f17ea
Olivier <Olivier@DESKTOP-6M4F80G>
2019-03-06 15:23:34 +0100

    Created Unity scenes

--- simple-ml-tests/PerceptronTests.cs
+++ simple-ml-tests/PerceptronTests.cs
@@ -10,13 +10,6 @@ namespace simple_ml_tests
         [TestMethod]
         public void ShouldOutputExpectedResults()
         {
-            //var input = new[]
-            //{
-            //    new[] { 1, 0.5, 0.1 },
-            //    new[] { 1, 1.5, 0.9 },
-            //    new[] { 1, 1.1, 1.2 }
-            //};
-
             var input = new[]{
                 new[] {1, 0.3, 1.5},
                 new[] {1, 0.9, -2.5},
@@ -25,15 +18,15 @@ namespace simple_ml_tests
             };
 
             var expectedOutput = new double[] {1, -1, 1, -1};
-            const double learningRate = 1;
-            const int nbEpochs = 10000;
+            const double learningRate = 0.1;
+            const int nbEpochs = 5000;
             
-            double[] weights = Perceptron.CreateModel(input.Length);
-            double[] output = Perceptron.TrainLinearClassification(weights, input, expectedOutput, learningRate, nbEpochs);
+            double[] model = Perceptron.CreateModel(input.Length);
+            model = Perceptron.TrainLinearClassification(model, input, expectedOutput, learningRate, nbEpochs);
 
             for (int i = 0; i < expectedOutput.Length; i++)
             {
-                Assert.AreEqual(expectedOutput[i], output[i]);
+                Assert.AreEqual(expectedOutput[i], model[i]);
             }
         }
     }
