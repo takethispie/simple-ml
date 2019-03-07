c6ea0fabf43862a76f348a096ef60d80355879ee
Nicolas Cornu <nicolas.demoncourt@gmail.com>
2019-03-07 14:04:15 +0100

    Linear Regression

---
+++ simple-ml-tests/SimpleLinearRegressionTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace simple_ml_tests
+{
+    [TestClass]
+    class SimpleLinearRegressionTests
+    {
+    }
+}
